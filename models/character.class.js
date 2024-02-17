7c3873df3f5114b5371251a65ba40b7a497d29d4
artir97 <85441675+artir97@users.noreply.github.com>
2024-02-17 17:20:12 +0100

    add character img

--- models/character.class.js
+++ models/character.class.js
@@ -1,5 +1,12 @@
 class Character extends MovableObject {
 
+
+    constructor(){
+        super();
+        this.loadImage('img/2_character_pepe/2_walk/W-21.png');
+    }
+
+
     jump() {
 
     }
--- models/world.class.js
+++ models/world.class.js
@@ -1,13 +1,20 @@
 class World {
 
-character = new Character();
-enemies = [
-    new Chicken(),
-    new Chicken(),
-    new Chicken()
-];
+    character = new Character();
+    enemies = [
+        new Chicken(),
+        new Chicken(),
+        new Chicken()
+    ];
+    ctx;
 
-draw() {
+    constructor(canvas) {
+        this.ctx = canvas.getContext('2d');
+        this.draw();
+    }
 
-}
+
+    draw() {
+        this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height);
+    }
 }
