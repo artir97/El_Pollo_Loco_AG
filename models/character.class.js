3437f244bdfdf0659a41427fbebb9be615772a1f
artir97 <85441675+artir97@users.noreply.github.com>
2024-02-19 17:12:09 +0100

    character can walk

--- models/character.class.js
+++ models/character.class.js
@@ -3,6 +3,7 @@ class Character extends MovableObject {
     y = 140;
     height = 300;
     width = 150;
+    speed = 10;
     IMAGES_WALKING = [
         'img/2_character_pepe/2_walk/W-21.png',
         'img/2_character_pepe/2_walk/W-22.png',
@@ -24,15 +25,26 @@ class Character extends MovableObject {
 
 
     animate() {
+        setInterval(()=> {
+            if (this.world.keyboard.RIGHT) {
+                this.x += this.speed;
+            }
+
+            if(this.world.keyboard.LEFT){
+                this.x -= this.speed;
+            }
+        }, 1000/60)
 
         setInterval(() => {
-            if (this.world.keyboard.RIGHT) {
+            if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+
+                // walk animation
                 let i = this.currentImage % this.IMAGES_WALKING.length;
                 let path = this.IMAGES_WALKING[i];
                 this.img = this.imageCache[path];
                 this.currentImage++;
             }
-        }, 100)
+        }, 1000/24)
     }
 
 
