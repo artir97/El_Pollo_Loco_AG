12a86b4e1c72f2e3c6090c522317904510597ea5
artir97 <85441675+artir97@users.noreply.github.com>
2024-02-22 13:17:55 +0100

    increase background

--- models/world.class.js
+++ models/world.class.js
@@ -11,14 +11,35 @@ class World {
         new Cloud()
     ];
     backgroundObjects = [
+        new BackgroundObject('img/5_background/layers/air.png', -719),
+        new BackgroundObject('img/5_background/layers/3_third_layer/2.png', -719),
+        new BackgroundObject('img/5_background/layers/2_second_layer/2.png', -719),
+        new BackgroundObject('img/5_background/layers/1_first_layer/2.png', -719),
+
         new BackgroundObject('img/5_background/layers/air.png', 0),
         new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 0),
         new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 0),
-        new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 0)
+        new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 0),
+
+        new BackgroundObject('img/5_background/layers/air.png', 719),
+        new BackgroundObject('img/5_background/layers/3_third_layer/2.png', 719),
+        new BackgroundObject('img/5_background/layers/2_second_layer/2.png', 719),
+        new BackgroundObject('img/5_background/layers/1_first_layer/2.png', 719),
+
+        new BackgroundObject('img/5_background/layers/air.png', 719*2),
+        new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 719*2),
+        new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 719*2),
+        new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 719*2),
+
+        new BackgroundObject('img/5_background/layers/air.png', 719*3),
+        new BackgroundObject('img/5_background/layers/3_third_layer/2.png', 719*3),
+        new BackgroundObject('img/5_background/layers/2_second_layer/2.png', 719*3),
+        new BackgroundObject('img/5_background/layers/1_first_layer/2.png', 719*3)
     ]
     canvas;
     ctx;
     keyboard;
+    camera_x = 0;
 
 
     constructor(canvas, keyboard) {
@@ -37,11 +58,15 @@ class World {
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
+        this.ctx.translate(this.camera_x, 0);
+
         this.addObjectsToMap(this.backgroundObjects);
         this.addObjectsToMap(this.clouds);
         this.addToMap(this.character)
         this.addObjectsToMap(this.enemies);
 
+        
+        this.ctx.translate(-this.camera_x, 0);
         let self = this;
         requestAnimationFrame(function () {
             self.draw();
