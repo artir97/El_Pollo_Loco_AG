de37ea3453f7100d6eb6c7e0d94301d31dc6eb98
artir97 <85441675+artir97@users.noreply.github.com>
2024-03-25 13:38:36 +0100

    ADD: death animation to chicken

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -9,12 +9,17 @@ class Chicken extends MovableObject {
         'img/3_enemies_chicken/chicken_normal/1_walk/2_w.png',
         'img/3_enemies_chicken/chicken_normal/1_walk/3_w.png'
     ];
+    IMAGES_DEAD = [
+        'img/3_enemies_chicken/chicken_normal/2_dead/dead.png'
+    ];
 
 
     constructor() {
         super();
         this.loadImage('img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
+        this.loadImage('img/3_enemies_chicken/chicken_normal/2_dead/dead.png');
         this.loadImages(this.IMAGES_WALKING);
+        this.loadImages(this.IMAGES_DEAD);
         this.x = 200 + 500 * Math.random();
         this.speed = 0.15 + Math.random() * 0.25;
         this.animate();
@@ -30,6 +35,10 @@ class Chicken extends MovableObject {
 
         setInterval(() => {
             this.playAnimation(this.IMAGES_WALKING);
+
+            if(this.isDead()){
+                this.playAnimation(this.IMAGES_DEAD);
+            }
         }, 100)
     }
 }
